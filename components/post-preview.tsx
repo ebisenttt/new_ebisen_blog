8bbbd9485e5ec343e74905e53ea2a681864fb61f
ebisenttt <vamos.nippon.2010@gmail.com>
2023-09-18 08:56:38 +0900

    previewをシンプルに

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -23,9 +23,6 @@ const PostPreview = ({
 }: Props) => {
   return (
     <div>
-      <div className="mb-5">
-        <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
       <h3 className="text-3xl mb-3 leading-snug">
         <Link
           as={`/posts/${slug}`}
@@ -38,8 +35,6 @@ const PostPreview = ({
       <div className="text-lg mb-4">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
     </div>
   )
 }
