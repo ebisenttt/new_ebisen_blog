880257307d810fb2446d80000f57b80185382fa9
ebisenttt <vamos.nippon.2010@gmail.com>
2023-09-18 09:16:13 +0900

    記事一覧の余白調整

--- components/posts.tsx
+++ components/posts.tsx
@@ -11,7 +11,7 @@ const Posts = ({ posts }: Props) => {
       <h2 className="mb-8 text-2xl md:text-5xl font-bold tracking-tighter leading-tight">
         Posts
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-8 md:gap-y-16 mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
