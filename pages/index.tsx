6d7ebaaf70e010c9011f80870d6ac42ca81f27de
ebisenttt <vamos.nippon.2010@gmail.com>
2023-09-18 09:00:53 +0900

    トップページをシンプルに

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
+import Posts from '../components/posts'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
 import Post from '../interfaces/post'
 
 type Props = {
@@ -23,17 +21,7 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          <Posts posts={morePosts} />
         </Container>
       </Layout>
     </>
