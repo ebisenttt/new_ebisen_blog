96c1467b07860d057a10770c32977d1263852c39
ebisenttt <vamos.nippon.2010@gmail.com>
2023-09-21 07:38:41 +0900

    ダークテーマを適用

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="ja">
+    <Html lang="ja-JP" className="dark">
       <Head />
-      <body>
+      <body className='dark:bg-gray-900 dark:text-slate-50'>
         <Main />
         <NextScript />
       </body>
