1acdf8ff3cd2bc3dd4796756dc0dbd380c65c31f
ebisenttt <vamos.nippon.2010@gmail.com>
2023-09-24 10:41:15 +0900

    eslint, prettier

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -8,26 +8,20 @@ type Props = {
   slug: string
 }
 
-const PostPreview = ({
-  title,
-  date,
-  slug,
-}: Props) => {
+const PostPreview = ({ title, date, slug }: Props) => {
   return (
-    <Card>
-      <h3 className="text-xl mb-3 leading-snug">
-        <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
-          className="hover:underline"
-        >
-          {title}
-        </Link>
-      </h3>
-      <div className="text-base mb-4">
-        <DateFormatter dateString={date} />
-      </div>
-    </Card>
+    <Link
+      as={`/posts/${slug}`}
+      href="/posts/[slug]"
+      className="hover:underline"
+    >
+      <Card>
+        <h3 className="text-xl mb-3 leading-snug">{title}</h3>
+        <div className="text-base mb-4">
+          <DateFormatter dateString={date} />
+        </div>
+      </Card>
+    </Link>
   )
 }
 
