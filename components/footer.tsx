613d9f96c2b6305614c22814f19aa1dc6895fc07
ebisenttt <vamos.nippon.2010@gmail.com>
2023-10-11 09:13:21 +0900

    フッターを変更

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,5 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
@@ -7,20 +7,28 @@ const Footer = () => {
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
+            ebisen blog.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              href="https://github.com/ebisenttt/new_ebisen_blog"
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              <Image
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                alt="github"
+                width={40}
+                height={40}
+                className="invert"
+              />
+            </a>
+            <a href="https://twitter.com/ebisenttt">
+              <Image
+                src="https://cdn.qiita.com/assets/brand_icons/icon-x_white-a2cf94da373d24e4cf6e4848a3a1c4a8.svg"
+                alt="x-twitter"
+                width={40}
+                height={40}
+              />
             </a>
           </div>
         </div>
