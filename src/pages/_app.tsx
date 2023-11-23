dcd86868f519e2d559432986cc8184125677fb41
ebisenttt <vamos.nippon.2010@gmail.com>
2023-11-23 12:17:25 +0900

    別のところでmetaタグが指定されていたので修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
 import type { AppProps } from 'next/app'
 import 'styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
-import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta property="og:image" content="https://www.ebisenttt.com/api/og" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
