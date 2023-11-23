65a1d7c4a0d49e06f8ac387701c54884906ea133
ebisenttt <vamos.nippon.2010@gmail.com>
2023-11-23 15:01:25 +0900

    OGPをPreview環境にも対応

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
 export const TITLE = 'ebisen blog.'
 export const CMS_NAME = 'Markdown'
-export const HOME_OG_IMAGE_URL = 'https://www.ebisen.com/api/og'
+export const HOME_OG_IMAGE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`
