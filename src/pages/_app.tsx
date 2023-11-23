4f8e89ccbdf3dc3bbf5fe91579a2d7274863a270
ebisenttt <vamos.nippon.2010@gmail.com>
2023-11-23 12:10:18 +0900

    metaタグでogを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app'
 import 'styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta property="og:image" content="https://www.ebisenttt.com/api/og" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
