819e07e0b0f2be570e9513d95deaf61fb94ce200
ebisenttt <vamos.nippon.2010@gmail.com>
2024-04-10 07:58:49 +0900

    表を表示するためにremark周りをアップデート

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -2,12 +2,14 @@ import { remark } from 'remark'
 import remarkEmbedder from '@remark-embedder/core'
 import html from 'remark-html'
 import { CodePenTransformer } from 'lib/embedInMarkdown'
+import gfm from 'remark-gfm'
 
 export default async function markdownToHtml(markdown: string) {
   const result = await remark()
     .use(remarkEmbedder, {
       transformers: [CodePenTransformer],
     })
+    .use(gfm)
     .use(html, { sanitize: false })
     .process(markdown)
   return result.toString()
