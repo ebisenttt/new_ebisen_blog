dc61a0d9000415f669d214bc8e67926834bb21ef
ebisen <vamos.nippon.2010@gmail.com>
2025-03-20 16:05:59 +0900

    CI導入 (#62)
    * eslintをv9にあげる
    
    * husky導入と型エラー解消
    
    * lintエラーが発生しないように調整
    
    * huskyの設定調整
    
    * eslintのignoreを設定してCIを追加
    
    * pre-push修正
    
    * pre-pushのlint対象にstaged以外も追加
    
    * update eslint-parser
    
    * remarkからunifiedに乗り換え
    
    * tsconfig修正
    
    * tscにcssをmoduleとして認識させる

---
+++ eslint.config.mjs
@@ -0,0 +1,114 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { defineConfig } from 'eslint/config'
+import unusedImports from 'eslint-plugin-unused-imports'
+import _import from 'eslint-plugin-import'
+import globals from 'globals'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  {
+    ignores: [
+      'node_modules',
+      '.next',
+      '.vercel',
+      'public',
+      '_drafts',
+      '_posts',
+      'tailwind.config.js',
+    ],
+  },
+  {
+    extends: compat.extends(
+      'plugin:react/recommended',
+      'prettier',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:@next/next/recommended',
+    ),
+
+    plugins: {
+      'unused-imports': unusedImports,
+      import: _import,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'unused-imports/no-unused-imports': 'error',
+
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'index', 'type'],
+          'newlines-between': 'always',
+
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next/*',
+              group: 'external',
+              position: 'before',
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ['react', 'next'],
+
+          alphabetize: {
+            orderImportKind: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['../', './'],
+        },
+      ],
+
+      '@next/next/no-duplicate-head': 'off', // 次のエラーが発生するため一時的にoff: context.getAncestor() is not a function
+      '@next/next/no-page-custom-font': 'off', // 次のエラーが発生するため一時的にoff: context.getAncestor() is not a function
+    },
+  },
+])
