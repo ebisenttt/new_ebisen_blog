0f1097cc9157060175ae7ae22d6ceff0e6697e58
ebisen <vamos.nippon.2010@gmail.com>
2025-05-30 08:53:17 +0900

    Google Analyticsのscriptを設置 (#74)
    * google analyticsのタグを設置
    
    * production環境のみgaを有効化
    
    * log出力削除

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
 export const TITLE = 'ebisen blog.'
 export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/og`
+export const GA_ID = 'G-T68PSH6LFR'
