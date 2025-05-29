2fb54853d7e0284788856f3a5dadecc7d5b0a742
ebisenttt <vamos.nippon.2010@gmail.com>
2025-05-30 08:25:08 +0900

    google analyticsのタグを設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
+
 import '@/styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-T68PSH6LFR" />
+    </>
+  )
 }
