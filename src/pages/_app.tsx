f06107977db7eb1021f5b9e6e17a2b3fa8e54131
ebisenttt <vamos.nippon.2010@gmail.com>
2025-05-30 08:50:05 +0900

    log出力削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 import { GA_ID } from '@/constants'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
   return (
     <>
       <Component {...pageProps} />
