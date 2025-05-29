ffd1b509496640b00ce48d337a1a903f92a43831
ebisenttt <vamos.nippon.2010@gmail.com>
2025-05-30 08:46:34 +0900

    production環境のみgaを有効化

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,16 @@ import '@/styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
 import { GoogleAnalytics } from '@next/third-parties/google'
 
+import { GA_ID } from '@/constants'
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  console.log('process.env.VERCEL_ENV', process.env.VERCEL_ENV)
   return (
     <>
       <Component {...pageProps} />
-      <GoogleAnalytics gaId="G-T68PSH6LFR" />
+      {process.env.VERCEL_ENV === 'production' && (
+        <GoogleAnalytics gaId={GA_ID} />
+      )}
     </>
   )
 }
