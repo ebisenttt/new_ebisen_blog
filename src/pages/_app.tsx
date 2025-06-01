d917a2e577eadfcf601ab58a65d30d80388edac1
ebisenttt <vamos.nippon.2010@gmail.com>
2025-06-02 07:54:02 +0900

    GAではなくGTMを設置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,14 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/next'
 import '@/styles/index.css'
 import 'prismjs/themes/prism-tomorrow.css'
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      {process.env.NEXT_PUBLIC_GA_ID && (
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+      {process.env.NEXT_PUBLIC_GTM_ID && (
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
       )}
       <Analytics />
     </>
