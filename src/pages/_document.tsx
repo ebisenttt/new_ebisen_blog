37d8614298aff5aadf31e8af2de57bd2899d9ee8
ebisenttt <vamos.nippon.2010@gmail.com>
2025-06-01 10:57:19 +0900

    Analyticsコンポーネントの配置場所を修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import { Analytics } from '@vercel/analytics/next'
-
 export default function Document() {
   return (
     <Html lang="ja-JP" className="dark">
@@ -9,7 +7,6 @@ export default function Document() {
       <body className="dark:bg-gray-900 dark:text-slate-50">
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   )
