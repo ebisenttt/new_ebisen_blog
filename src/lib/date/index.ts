71993d442b809725ecfe11b42fae923f2359c451
ebisenttt <vamos.nippon.2010@gmail.com>
2025-06-14 09:59:57 +0900

    コメント追加

--- src/lib/date/index.ts
+++ src/lib/date/index.ts
@@ -1,5 +1,7 @@
 import { parseISO as _parseISO, format as _format } from 'date-fns'
 import { ja } from 'date-fns/locale'
+// NOTE:@date-fns/tzからimportできないバグがある
+// https://github.com/date-fns/tz/issues/43
 import { tz } from '@date-fns/tz/tz'
 
 export const parseISO = (dateString: string): Date => {
