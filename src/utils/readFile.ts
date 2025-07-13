efc8591793a1d704c98cabd2149be8289ad8dfba
ebisenttt <vamos.nippon.2010@gmail.com>
2025-07-13 15:47:29 +0900

    変更を元に戻してlogを仕込む

--- src/utils/readFile.ts
+++ src/utils/readFile.ts
@@ -1,9 +1,12 @@
 import fs from 'fs'
 
 export const readFile = (path: string) => {
+  console.log({ path })
+
   try {
     return fs.readFileSync(path, 'utf8')
   } catch {
+    console.log('File not found:', path)
     return null
   }
 }
